b5fad3c1045599b5939f0a7cc2a1aeab295baecc
kennystewart <kennystwork@gmail.com>
2022-10-02 17:05:20 -0700

    Initial Commit

---
+++ Videlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Videlay
+//
+//  Created by June Kim on 10/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
